ddfd6cc2dc20034aaec6400405ff7dbc7f797b95
unknown <you@example.com>
2021-05-20 21:08:11 +0300

    20/5/2021__g1

--- database/migrations/2021_04_07_152514_create_charities_table.php
+++ database/migrations/2021_04_07_152514_create_charities_table.php
@@ -15,25 +15,25 @@ class CreateCharitiesTable extends Migration
     {
         Schema::create('charities', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
-            $table->string('name_en')->nullable();
-            $table->string('name_ar')->nullable();
+            $table->unsignedBigInteger('user_id');//
+            $table->string('name_en')->nullable();//
+            $table->string('name_ar')->nullable();//
             $table->unsignedInteger('license')->unique();
             $table->unsignedBigInteger('country_id')->nullable();
             $table->unsignedBigInteger('city_id')->nullable();
             $table->unsignedBigInteger('state_id')->nullable();
-            $table->string('address_ar')->nullable();
-            $table->string('address_en')->nullable();
-            $table->string('whatsapp')->nullable();
-            $table->string('facebook')->nullable();
-            $table->string('email')->nullable();
-            $table->string('phone')->nullable();
-            $table->string('mobile')->nullable();
-            $table->string('logo_ar')->nullable();
-            $table->string('logo_en')->nullable();
-            $table->string('cover')->nullable();
-            $table->text('info_ar')->nullable();
-            $table->text('info_en')->nullable();
+            $table->string('address_ar')->nullable();//
+            $table->string('address_en')->nullable();//
+            $table->string('whatsapp')->nullable();//
+            $table->string('facebook')->nullable();//
+            $table->string('email')->nullable();//
+            $table->string('phone')->nullable();//
+            $table->string('mobile')->nullable();//
+            $table->string('logo_ar')->nullable();//
+            $table->string('logo_en')->nullable();//
+            $table->string('cover')->nullable();//
+            $table->text('info_ar')->nullable();//
+            $table->text('info_en')->nullable();//
             $table->timestamps();
         });
     }
--- resources/views/Charity/crup.blade.php
+++ resources/views/Charity/crup.blade.php
@@ -4,12 +4,7 @@
 <li class="nav-item">
     <a class="nav-link active" href="#">Active</a>
 </li>
-<li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-</li>
-<li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-</li>
+
 <li class="nav-item">
     <a class="nav-link disabled" href="#">Disabled</a>
 </li>
@@ -27,6 +22,65 @@
                     @if (!empty($charity))
                     @method('PUT')
                     @endif
+
+                    <!--cover-->
+                    <div class="row">
+                        <div class=" @if (!empty($charity)) col-md-2 @else col-md-4 @endif col-form-label "
+                            style="text-align:start">
+                            <label for="cover">{{ __('Cover') }}:</label>
+                        </div>
+                        @if (!empty($charity))
+                        <div class="col-md-2 col-form-label " style="text-align:start">
+                            <img src="{{ asset('storage/charities/cover/' . $charity->cover) }}"
+                                class="img-fluid img-thumbnail">
+                        </div>
+                        @endif
+                        <div class="col-8">
+                            <input class="form-control" type="file" name="cover" @if (empty($charity)) required @endif>
+                        </div>
+                    </div>
+
+                    <!--logo_ar-->
+                    <div class="row">
+                        <div class=" @if (!empty($charity)) col-md-2 @else col-md-4 @endif col-form-label "
+                            style="text-align:start">
+                            <label for="logo_ar">{{ __('Logo_AR') }}:</label>
+                        </div>
+                        @if (!empty($charity))
+                        <div class="col-md-2 col-form-label " style="text-align:start">
+                            <img src="{{ asset('storage/charities/logo/' . $charity->logo_ar) }}"
+                                class="img-fluid img-thumbnail">
+                        </div>
+                        @endif
+                        <div class="col-8">
+                            <input class="form-control" type="file" name="logo_ar" @if (empty($charity)) required
+                                @endif>
+                        </div>
+                    </div>
+
+
+                    <!--logo_en-->
+                    <div class="row">
+                        <div class=" @if (!empty($charity)) col-md-2 @else col-md-4 @endif col-form-label "
+                            style="text-align:start">
+                            <label for="logo_en">{{ __('Logo_EN') }}:</label>
+                        </div>
+
+                        @if (!empty($charity))
+                        <div class="col-md-2 col-form-label " style="text-align:start">
+                            <img src="{{ asset('storage/charities/logo/' . $charity->logo_en) }}"
+                                class="img-fluid img-thumbnail">
+                        </div>
+                        @endif
+
+                        <div class="col-8">
+                            <input class="form-control" type="file" name="logo_en" @if (empty($charity)) required
+                                @endif>
+                        </div>
+                    </div>
+
+
+                    <!--name-->
                     <div class="row">
                         <div class="col-md-2 col-form-label text-md-left">
                             <label for="name_ar">{{ __('Arabic Name') }}:</label>
@@ -43,45 +97,126 @@
                             {{ $charity->name_en }}@else{{old('name_en')}} @endif>
                         </div>
                     </div>
-                    <div class="row">
+
+
+                    <!--license   ماخلصت-->
+                    <!-- <div class="row">
                         <div class=" @if (!empty($charity)) col-md-2 @else col-md-4 @endif col-form-label "
                             style="text-align:start">
-                            <label for="logo_ar">{{ __('logo_ar') }}:</label>
+                            <label for="license">{{ __('license') }}:</label>
                         </div>
                         @if (!empty($charity))
                         <div class="col-md-2 col-form-label " style="text-align:start">
-                            <img src="{{ asset('storage/charities/logo/' . $charity->logo_ar) }}"
+                            <img src="{{ asset('storage/charities/logo/' . $charity->license) }}"
                                 class="img-fluid img-thumbnail">
                         </div>
                         @endif
                         <div class="col-8">
-                            <input class="form-control" type="file" name="logo_ar" @if (empty($charity)) required
+                            <input class="form-control" type="file" name="license" @if (empty($charity)) required
                                 @endif>
                         </div>
-                    </div>
+                    </div> -->
+
+
+                   
+                    <!--email-->
                     <div class="row">
-                        <div class=" @if (!empty($charity)) col-md-2 @else col-md-4 @endif col-form-label "
-                            style="text-align:start">
-                            <label for="logo_en">{{ __('logo_en') }}:</label>
+                        <div class="col-md-2 col-form-label text-md-left">
+                            <label for="email">{{ __('Email') }}:</label>
                         </div>
-
-                        @if (!empty($charity))
-                        <div class="col-md-2 col-form-label " style="text-align:start">
-                            <img src="{{ asset('storage/charities/logo/' . $charity->logo_en) }}"
-                                class="img-fluid img-thumbnail">
-                        </div>
-                        @endif
-
-                        <div class="col-8">
-                            <input class="form-control" type="file" name="logo_en" @if (empty($charity)) required
-                                @endif>
+                        <div class="col-4"><input class="form-control" type="text" name="email"
+                                value=@if(!empty($charity) && old('email', $charity->email))
+                            {{ $charity->email }}@else {{old('email')}} @endif>
                         </div>
                     </div>
+
+
+                    <!--phone-->
                     <div class="row">
-                        <div class="col--md-2 col-form-label">
-
+                        <div class="col-md-2 col-form-label text-md-left">
+                            <label for="phone">{{ __('Phone') }}:</label>
+                        </div>
+                        <div class="col-4"><input class="form-control" type="text" name="phone"
+                                value=@if(!empty($charity) && old('phone', $charity->phone))
+                            {{ $charity->phone }}@else {{old('phone')}} @endif>
                         </div>
                     </div>
+
+                     <!--mobile-->
+                    <div class="row">
+                        <div class="col-md-2 col-form-label text-md-left">
+                            <label for="mobile">{{ __('Mobile') }}:</label>
+                        </div>
+                        <div class="col-4"><input class="form-control" type="text" name="mobile"
+                                value=@if(!empty($charity) && old('mobile', $charity->mobile))
+                            {{ $charity->mobile }}@else {{old('mobile')}} @endif>
+                        </div>
+                    </div>
+
+
+                    <!--whatsapp-->
+                    <div class="row">
+                        <div class="col-md-2 col-form-label text-md-left">
+                            <label for="whatsapp">{{ __('Whatsapp') }}:</label>
+                        </div>
+                        <div class="col-4"><input class="form-control" type="text" name="whatsapp"
+                                value=@if(!empty($charity) && old('whatsapp', $charity->whatsapp))
+                            {{ $charity->whatsapp }}@else {{old('whatsapp')}} @endif>
+                        </div>
+                    </div>
+
+
+                    <!--facebook-->
+                    <div class="row">
+                        <div class="col-md-2 col-form-label text-md-left">
+                            <label for="facebook">{{ __('Facebook') }}:</label>
+                        </div>
+                        <div class="col-4"><input class="form-control" type="text" name="facebook"
+                                value=@if(!empty($charity) && old('facebook', $charity->facebook))
+                            {{ $charity->facebook }}@else {{old('facebook')}} @endif>
+                        </div>
+                    </div>
+
+
+                   <!-- address-->
+                    <div class="row">
+                        <div class="col-md-2 col-form-label text-md-left">
+                            <label for="address_ar">{{ __('Address_AR') }}:</label>
+                        </div>
+                        <div class="col-4"><input class="form-control" type="text" name="address_ar"
+                                value=@if(!empty($charity) && old('address_ar', $charity->address_ar))
+                            {{ $charity->address_ar }}@else {{old('address_ar')}} @endif>
+                        </div>
+                        <div class="col-md-2 col-form-label text-md-left">
+                            <label for="address_en">{{ __('Address_EN') }}:</label>
+                        </div>
+                        <div class="col-4"><input class="form-control" type="text" name="address_en"
+                                value=@if(!empty($charity) && old('address_en', $charity->address_en))
+                            {{ $charity->address_en }}@else{{old('address_en')}} @endif>
+                        </div>
+                    </div>
+
+
+                  <!--info-->
+                    <div class="row">
+                        <div class="col-md-2 col-form-label text-md-left">
+                            <label for="info_ar">{{ __('Info_AR') }}:</label>
+                        </div>
+                        <div class="col-4"><input class="form-control" type="text" name="info_ar"
+                                value=@if(!empty($charity) && old('info_ar', $charity->info_ar))
+                            {{ $charity->info_ar }}@else {{old('info_ar')}} @endif>
+                        </div>
+                        <div class="col-md-2 col-form-label text-md-left">
+                            <label for="info_en">{{ __('Info_EN') }}:</label>
+                        </div>
+                        <div class="col-4"><input class="form-control" type="text" name="info_en"
+                                value=@if(!empty($charity) && old('info_en', $charity->info_en))
+                            {{ $charity->info_en }}@else {{old('info_en')}} @endif>
+                        </div>
+
+                    </div>
+
+
                     <div class="row">
                         <div class="col-6 text-md-center">
                             <button type="submit" class="btn btn-primary">{{ __('Submit') }}</button>
