1042200751bd79fa14c7da4972b55695301ed635
rafah mnjd <rafahmnjd@gmail.com>
2021-06-24 07:19:14 +0300

    dataBaseMigration rafah-24-6-7:19 AM

--- resources/views/charities/crup.blade.php
+++ resources/views/charities/crup.blade.php
@@ -1,11 +1,9 @@
 @extends('layouts.app')
-
-
 @section('content')
 <div class="row justify-content-center">
     <div class="col-md">
         <div class="card">
-            <div class="card-header"></div>
+            <div class="card-header"><h5 class="card-title">{{__("Charity Info")}}</h5></div>
             <div class="card-body">
                 <form action="{{empty($charity)? route('charities.store') : route('charities.update',$charity)}}"
                     method="Post" enctype="multipart/form-data">
@@ -16,48 +14,54 @@
 
                     <!--cover-->
                     <div class="row">
-                        <div class=" @if (!empty($charity)) col-md-2 @else col-md-4 @endif col-form-label "
-                            style="text-align:start">
+                        <div class=" @if (!empty($charity)) col-md-2 @else col-md-4 @endif col-form-label ">
                             <label for="cover">{{ __('Cover') }}:</label>
                         </div>
                         @if (!empty($charity))
-                        <div class="col-md-2 col-form-label " style="text-align:start">
-                            <img src="{{ asset(config('path.covers').$charity->cover) }}" class="img-fluid img-thumbnail">
+                        <div class="col-md-2 col-form-label " >
+                            <img src="{{ asset(config('path.covers').$charity->cover) }}"
+                                class="img-fluid img-thumbnail">
                         </div>
                         @endif
-                        <div class="col-8">
-                            <input class="form-control" type="file" name="cover" @if (empty($charity)) required @endif>
+                        <div class="col-8 input-group mt-3">
+                            <div class="custom-file">
+                                <input type="file" class="custom-file-input" name="cover" @if(empty($charity)) required
+                                    @endif>
+                                <label class="custom-file-label text-left">No file chosen</label>
+                            </div>
                         </div>
                     </div>
 
                     <!--logo-->
                     <div class="row">
                         <div class=" @if (!empty($charity)) col-md-2 @else col-md-4 @endif col-form-label "
-                            style="text-align:start">
+                            >
                             <label for="logo">{{ __('Logo') }}:</label>
                         </div>
                         @if (!empty($charity))
-                        <div class="col-md-2 col-form-label " style="text-align:start">
+                        <div class="col-md-2 col-form-label " >
                             <img src="{{ asset(config('path.ch_logo').$charity->logo) }}"
                                 class="img-fluid img-thumbnail">
                         </div>
                         @endif
-                        <div class="col-8">
-                            <input class="form-control" type="file" name="logo" @if (empty($charity)) required
-                                @endif>
+                        <div class="col-8 input-group mt-3">
+                            <div class="custom-file">
+                                <input type="file" class="custom-file-input" name="logo" @if (empty($charity)) required @endif>
+                                <label class="custom-file-label text-left">No file chosen</label>
+                            </div>
                         </div>
                     </div>
 
 
                     <!--name-->
                     <div class="row">
-                        <div class="col-md-2 col-form-label text-md-left">
+                        <div class="col-md-2 col-form-label  ">
                             <label for="name_ar">{{ __('Arabic Name') }}:</label>
                         </div>
                         <div class="col-4"><input class="form-control" type="text" name="name_ar"
                                 value=@if(!empty($charity))"{{$charity->name_ar}}"@else "{{old('name_ar')}}" @endif>
                         </div>
-                        <div class="col-md-2 col-form-label text-md-left">
+                        <div class="col-md-2 col-form-label  ">
                             <label for="name_en">{{ __('English Name') }}:</label>
                         </div>
                         <div class="col-4"><input class="form-control" type="text" name="name_en"
@@ -69,11 +73,11 @@
                     <!--license   ماخلصت-->
                     <!-- <div class="row">
                         {{-- <div class=" @if (!empty($charity)) col-md-2 @else col-md-4 @endif col-form-label " --}}
-                            style="text-align:start">
+                            >
                             {{-- <label for="license">{{ __('license') }}:</label> --}}
                         </div>
                         {{-- @if (!empty($charity)) --}}
-                        <div class="col-md-2 col-form-label " style="text-align:start">
+                        <div class="col-md-2 col-form-label " >
                             {{-- <img src="{{ asset('storage/charities/logo/' . $charity->license) }}" --}}
                                 class="img-fluid img-thumbnail">
                         </div>
@@ -88,7 +92,7 @@
 
                     <!--email-->
                     <div class="row">
-                        <div class="col-md-2 col-form-label text-md-left">
+                        <div class="col-md-2 col-form-label  ">
                             <label for="email">{{ __('Email') }}:</label>
                         </div>
                         <div class="col-4"><input class="form-control" type="text" name="email"
@@ -99,7 +103,7 @@
 
                     <!--phone-->
                     <div class="row">
-                        <div class="col-md-2 col-form-label text-md-left">
+                        <div class="col-md-2 col-form-label  ">
                             <label for="phone">{{ __('Phone') }}:</label>
                         </div>
                         <div class="col-4"><input class="form-control" type="text" name="phone"
@@ -107,9 +111,9 @@
                         </div>
                     </div>
 
-                     <!--mobile-->
+                    <!--mobile-->
                     <div class="row">
-                        <div class="col-md-2 col-form-label text-md-left">
+                        <div class="col-md-2 col-form-label  ">
                             <label for="mobile">{{ __('Mobile') }}:</label>
                         </div>
                         <div class="col-4"><input class="form-control" type="text" name="mobile"
@@ -120,7 +124,7 @@
 
                     <!--whatsapp-->
                     <div class="row">
-                        <div class="col-md-2 col-form-label text-md-left">
+                        <div class="col-md-2 col-form-label  ">
                             <label for="whatsapp">{{ __('Whatsapp') }}:</label>
                         </div>
                         <div class="col-4"><input class="form-control" type="text" name="whatsapp"
@@ -131,7 +135,7 @@
 
                     <!--facebook-->
                     <div class="row">
-                        <div class="col-md-2 col-form-label text-md-left">
+                        <div class="col-md-2 col-form-label  ">
                             <label for="facebook">{{ __('Facebook') }}:</label>
                         </div>
                         <div class="col-4"><input class="form-control" type="text" name="facebook"
@@ -140,32 +144,34 @@
                     </div>
 
 
-                   <!-- address-->
+                    <!-- address-->
                     <div class="row">
-                        <div class="col-md-2 col-form-label text-md-left">
+                        <div class="col-md-2 col-form-label  ">
                             <label for="address_ar">{{ __('Address_AR') }}:</label>
                         </div>
                         <div class="col-4"><input class="form-control" type="text" name="address_ar"
-                                value=@if(!empty($charity))"{{$charity->address_ar}}" @else "{{old('address_ar')}}" @endif>
+                                value=@if(!empty($charity))"{{$charity->address_ar}}" @else "{{old('address_ar')}}"
+                                @endif>
                         </div>
-                        <div class="col-md-2 col-form-label text-md-left">
+                        <div class="col-md-2 col-form-label  ">
                             <label for="address_en">{{ __('Address_EN') }}:</label>
                         </div>
                         <div class="col-4"><input class="form-control" type="text" name="address_en"
-                                value=@if(!empty($charity) )"{{$charity->address_en}}" @else "{{old('address_en')}}" @endif>
+                                value=@if(!empty($charity) )"{{$charity->address_en}}" @else "{{old('address_en')}}"
+                                @endif>
                         </div>
                     </div>
 
 
-                  <!--info-->
+                    <!--info-->
                     <div class="row">
-                        <div class="col-md-2 col-form-label text-md-left">
+                        <div class="col-md-2 col-form-label  ">
                             <label for="info_ar">{{ __('Info_AR') }}:</label>
                         </div>
                         <div class="col-4"><input class="form-control" type="text" name="info_ar"
                                 value=@if(!empty($charity)) "{{$charity->info_ar}}" @else "{{old('info_ar')}}" @endif>
                         </div>
-                        <div class="col-md-2 col-form-label text-md-left">
+                        <div class="col-md-2 col-form-label  ">
                             <label for="info_en">{{ __('Info_EN') }}:</label>
                         </div>
                         <div class="col-4"><input class="form-control" type="text" name="info_en"
