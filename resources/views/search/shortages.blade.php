6824d078ca673f60c98c4c8d9b4daa3a710b9f1f
unknown <you@example.com>
2021-07-04 15:00:47 +0300

    4/7/2021_g4

--- resources/views/search/shortages.blade.php
+++ resources/views/search/shortages.blade.php
@@ -3,5 +3,60 @@
 @section('search_form_action',route('search.getShortages'))
 @section('middile')
 
-
+    <div class="result-body">
+        <div class="table-responsive">
+            <table class="table widget-26">
+                @foreach ($shortages as $shortage)
+                    <tbody>
+                        <tr>
+                            <td>
+                                <div class="widget-26-job-emp-img">
+                                    <img src="{{ asset(config('path.ch_logo') . $shortage->charity->logo) }}" alt="" />
+                                </div>
+                            </td>
+                            <td>
+                                <div class="widget-26-job-title">
+                                    <a href="{{ route('charities.show', $shortage->charity) }}">
+                                        <span>
+                                            @if (config('app.locale') == 'ar')
+                                                {{ $shortage->item->name_ar }}
+                                                <p class="m-0"><a href="{{ route('charities.shortage', $shortage->charity) }}"
+                                                        class="employer-name">{{$shortage->item->name_en }}</a></p>
+                                            @else
+                                                {{ $shortage->item->name_en }}
+                                                <p class="m-0"><a href="{{ route('charities.shortage', $shortage->charity) }}"
+                                                        class="employer-name">{{ $shortage->item->name_ar }}</a></p>
+                                            @endif
+                                        </span>
+                                    </a>
+                                </div>
+                            </td>
+                             <td>
+                                <div class="widget-26-job-title">
+                                        <a href="{{ route('charities.show', $shortage->charity) }}">
+                                            <span>
+                                                @if (config('app.locale') == 'ar')
+                                                    {{ $shortage->charity->name_ar }}
+                                                    <p class="m-0"><a href="{{ route('charities.show', $shortage->charity) }}"
+                                                            class="employer-name">{{ $shortage->charity->name_en }}</a></p>
+                                                @else
+                                                    {{ $shortage->charity->name_en }}
+                                                    <p class="m-0"><a href="{{ route('charities.show', $shortage->charity) }}"
+                                                            class="employer-name">{{ $shortage->charity->name_ar }}</a></p>
+                                                @endif
+                                            </span>
+                                        </a>
+                                </div>
+                            </td>
+                            <td>
+                                <div class="widget-26-job-info">
+                                        <p class="type m-0">{{ $shortage->quantity}}</p>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                @endforeach
+            </table>
+        </div>
+    </div>
 @endsection
---
+++ resources/views/search/surpluses.blade.php
@@ -0,0 +1,62 @@
+@extends('search.show')
+@section('search_class',__("Surpluses"))
+@section('search_form_action',route('search.getSurpluses'))
+@section('middile')
+
+     <div class="result-body">
+        <div class="table-responsive">
+            <table class="table widget-26">
+                @foreach ($surpluses as $surplus)
+                    <tbody>
+                        <tr>
+                            <td>
+                                <div class="widget-26-job-emp-img">
+                                    <img src="{{ asset(config('path.ch_logo') . $surplus->charity->logo) }}" alt="" />
+                                </div>
+                            </td>
+                            <td>
+                                <div class="widget-26-job-title">
+                                    <a href="{{ route('charities.show', $surplus->charity) }}">
+                                        <span>
+                                            @if (config('app.locale') == 'ar')
+                                                {{ $surplus->item->name_ar }}
+                                                <p class="m-0"><a href="{{ route('charities.surplus', $surplus->charity) }}"
+                                                        class="employer-name">{{$surplus->item->name_en }}</a></p>
+                                            @else
+                                                {{ $surplus->item->name_en }}
+                                                <p class="m-0"><a href="{{ route('charities.surplus', $surplus->charity) }}"
+                                                        class="employer-name">{{ $surplus->item->name_ar }}</a></p>
+                                            @endif
+                                        </span>
+                                    </a>
+                                </div>
+                            </td>
+                             <td>
+                                <div class="widget-26-job-title">
+                                        <a href="{{ route('charities.show', $surplus->charity) }}">
+                                            <span>
+                                                @if (config('app.locale') == 'ar')
+                                                    {{ $surplus->charity->name_ar }}
+                                                    <p class="m-0"><a href="{{ route('charities.show', $surplus->charity) }}"
+                                                            class="employer-name">{{ $surplus->charity->name_en }}</a></p>
+                                                @else
+                                                    {{ $surplus->charity->name_en }}
+                                                    <p class="m-0"><a href="{{ route('charities.show', $surplus->charity) }}"
+                                                            class="employer-name">{{ $surplus->charity->name_ar }}</a></p>
+                                                @endif
+                                            </span>
+                                        </a>
+                                </div>
+                            </td>
+                            <td>
+                                <div class="widget-26-job-info">
+                                        <p class="type m-0">{{ $surplus->quantity}}</p>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                @endforeach
+            </table>
+        </div>
+    </div>
+@endsection
