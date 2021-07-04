2921bb3f8372ca45b1d319f35c6a13bb46657d41
rafah mnjd <rafahmnjd@gmail.com>
2021-07-04 11:44:11 +0300

    mearge search controller

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -1,15 +1,11 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\{
-    CharityJob,
-    Charity,
-    Project,
-    Shortage,
-    Item
-};
 
-use Illuminate\Http\Request;
+use App\Models\Charity;
+use App\Models\CharityJob;
+use App\Models\Project;
+use App\Models\Shortage;
 
 class SearchController extends Controller
 {
@@ -22,10 +18,33 @@ class SearchController extends Controller
     {
         # code...
         $jobs = CharityJob::latest()->paginate(10);
-        return view('search.jobs',compact('jobs'));
+        return view('search.jobs', compact('jobs'));
     }
+    public function jobsTitleAtoZ($lang)
+    {
+        # code...
+        if ($lang == 'ar') {
+            $jobs = CharityJob::orderBy('job_title_ar')->paginate(10);
+        } else {
+            $jobs = CharityJob::orderBy('job_title_en')->paginate(10);
+        }
 
-        /**
+        return view('search.jobs', compact('jobs'));
+    }
+    public function getJobs($data)
+    {
+        # code...
+        $jobs = CharityJob::where('job_title_ar', 'like', '%' . $data . '%')
+            ->orWhere('job_title_en', 'like', '%' . $data . '%')
+            ->orWhere('job_details_ar', 'like', '%' . $data . '%')
+            ->orWhere('job_details_en', 'like', '%' . $data . '%')
+            ->orWhere('location_ar', 'like', '%' . $data . '%')
+            ->orWhere('location_en', 'like', '%' . $data . '%')
+        // ->orWhere('tag', 'like', '%' . $data . '%')
+            ->paginate(10);
+        return view('search.jobs', compact('jobs'));
+    }
+    /**
      * Undocumented function
      *
      * @return void
@@ -34,9 +53,45 @@ class SearchController extends Controller
     {
         # code...
         $charities = Charity::latest()->paginate(10);
-        return view('search.charities',compact('charities'));
+        return view('search.charities', compact('charities'));
     }
-        /**
+    public function charitiesNameAtoZ($lang)
+    {
+        # code...
+        if ($lang == 'ar') {
+            $charities = Charity::orderBy('name_ar')->paginate(10);
+        } else {
+            $charities = Charity::orderBy('name_en')->paginate(10);
+        }
+        return view('search.charities', compact('charities'));
+    }
+    public function charitiesAddressAtoZ($lang)
+    {
+        # code...
+        if ($lang == 'ar') {
+            $charities = Charity::orderBy('address_ar')->paginate(10);
+        } else {
+            $charities = Charity::orderBy('address_en')->paginate(10);
+        }
+        return view('search.charities', compact('charities'));
+    }
+    public function getCharities($data)
+    {
+        # code...
+        $charities = Charity::where('job_title_ar', 'like', '%' . $data . '%')
+            ->orWhere('name_en', 'like', '%' . $data . '%')
+            ->orWhere('name_ar', 'like', '%' . $data . '%')
+            ->orWhere('address_ar', 'like', '%' . $data . '%')
+            ->orWhere('address_en', 'like', '%' . $data . '%')
+            ->orWhere('info_ar', 'like', '%' . $data . '%')
+            ->orWhere('info_en', 'like', '%' . $data . '%')
+            ->paginate(10);
+
+        return view('search.charities', compact('charities'));
+
+    }
+
+    /**
      * Undocumented function
      *
      * @return void
@@ -45,9 +100,32 @@ class SearchController extends Controller
     {
         # code...
         $projects = Project::latest()->paginate(10);
-        return view('search.projects',compact('projects'));
+        return view('search.projects', compact('projects'));
     }
-        /**
+        public function projectsNameAtoZ($lang)
+    {
+        # code...
+        if ($lang == 'ar') {
+             $projects = Project::orderBy('title_ar')->paginate(10);
+        } else {
+             $projects = Project::orderBy('title_en')->paginate(10);
+        }
+        return view('search.projects', compact('projects'));
+    }
+    public function getProjects($data)
+    {
+
+        # code...
+        $projects = Project::where('job_title_ar', 'like', '%' . $data . '%')
+            ->orWhere('title_en', 'like', '%' . $data . '%')
+            ->orWhere('title_ar', 'like', '%' . $data . '%')
+            ->orWhere('text_ar', 'like', '%' . $data . '%')
+            ->orWhere('text_en', 'like', '%' . $data . '%')
+            ->paginate(10);
+        return view('search.projects', compact('projects'));
+
+    }
+    /**
      * Undocumented function
      *
      * @return void
@@ -55,10 +133,32 @@ class SearchController extends Controller
     public function shortages()
     {
         # code...
-        $shortages = Shortage::where('type','min')->latest()->paginate(10);
-        return view('search.shortages',compact('shortages'));
+        $shortages = Shortage::where('type', 'min')->latest()->paginate(10);
+        return view('search.shortages', compact('shortages'));
     }
-        /**
+    public function shortagesNameAtoZ($lang)
+    {
+        # code...
+        if ($lang == 'ar') {
+             $shortages = Shortage::orderBy('title_ar')->paginate(10);
+        } else {
+             $shortages = Shortage::orderBy('title_en')->paginate(10);
+        }
+        return view('search.shortages', compact('shortages'));
+    }
+    
+    public function getShortages($data)
+    {
+        # code...
+        $shortages = Shortage::with('Item')->where('type','min')->wherehas('Item', function($q)use($data){
+            $q->where('name_en', 'like', '%' . $data . '%')
+            ->orWhere('name_ar', 'like', '%' . $data . '%');
+        })
+        ->paginate(10);
+        return view('search.shortages', compact('shortages'));
+
+    }
+    /**
      * Undocumented function
      *
      * @return void
@@ -66,17 +166,9 @@ class SearchController extends Controller
     public function surpluses()
     {
         # code...
-        $shortages = Shortage::where('type','plus')->latest()->paginate(10);
-        return view('search.shortages',compact('shortages'));
+        $shortages = Shortage::where('type', 'plus')->latest()->paginate(10);
+        return view('search.shortages', compact('shortages'));
     }
-
-
-    public function getJobs($data)
-    {
-        # code...
-        $jobs=CharityJob::where('');
-    }
-
 
     public function show()
     {
